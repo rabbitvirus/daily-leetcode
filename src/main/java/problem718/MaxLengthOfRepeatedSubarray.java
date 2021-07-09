ac5a2cfbfd62f9805f9b6b5dcf79b5ef81a58ce2
rabbitvirus <kamil.poprawa13@gmail.com>
2021-07-09 09:17:58 +0200

    Problem 718: optimization

--- src/main/java/problem718/MaxLengthOfRepeatedSubarray.java
+++ src/main/java/problem718/MaxLengthOfRepeatedSubarray.java
@@ -6,19 +6,23 @@ public final class MaxLengthOfRepeatedSubarray {
         final int n1 = nums1.length;
         final int n2 = nums2.length;
 
+        int[] cur = new int[n1 + 1];
         int[] prev = new int[n1 + 1];
         int max = 0;
 
         for (int i = n2 - 1; i >= 0; i--) {
-            final int[] cur = new int[n1 + 1];
             for (int j = n1 - 1; j >= 0; j--) {
                 if (nums1[j] == nums2[i]) {
                     cur[j] = 1 + prev[j + 1];
                     if (cur[j] > max)
                         max = cur[j];
+                } else {
+                    cur[j] = 0;
                 }
             }
+            var tmp = prev;
             prev = cur;
+            cur = tmp;
         }
 
         return max;
